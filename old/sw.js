9af18d309764bfb017d9f896bbc564e30c48596f
nahyun824-sys <nahyun824-sys@users.noreply.github.com>
2026-08-02 23:09:05 +0900

    - 다른사람이 내 마인드 맵을 공유받아서 들어왔는데 가끔  안보이는 문제 고쳐줘 - 마인드맵을 엑셀로 다운로드 가능하게 해줘 - 마인드맵 노드 아래에 금액 넣을 수 있는 것처럼 메모 넣을 수 있게 해줘 - 디비 연결이 끊기는 문제 해결해줘 - 디비 연결이 끊기면 디비와 연결이 끊겼다고 오른쪽 아래 작게 붉은 글씨로 경고 문구 써줘

--- old/sw.js
+++ old/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mindtrip-v41';
+const CACHE = 'mindtrip-v40';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
